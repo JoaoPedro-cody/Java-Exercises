996148226ad90ad9a2362d2e34644e84b404fb90
Cody <ferreiradasilvajoaopedro730@gmail.com>
2024-12-16 04:48:21 -0300

    Exercice Conta Bacncaria

---
+++ src/Application/Banco.java
@@ -0,0 +1,41 @@
+package Application;
+
+import Entities.ContaBancaria;
+
+import java.util.Scanner;
+
+public class Banco {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your details: ");
+        System.out.print("Account Number: ");
+        int accountNumber = sc.nextInt();
+        System.out.print("Holder: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+        System.out.print("Balance: ");
+        double balance = sc.nextDouble();
+        ContaBancaria conta = new ContaBancaria(accountNumber, holder, balance);
+
+        System.out.println();
+        System.out.println(conta);
+
+        System.out.println();
+        System.out.print("Deposit balance: ");
+        double depositValue = sc.nextDouble();
+        conta.deposit(depositValue);
+
+        System.out.println();
+        System.out.println(conta);
+
+        System.out.println();
+        System.out.print("withdraw balance: ");
+        double withdrawValue = sc.nextDouble();
+        conta.withdraw(withdrawValue);
+
+        System.out.println();
+        System.out.println(conta);
+
+    }
+}
---
+++ src/Entities/ContaBancaria.java
@@ -0,0 +1,43 @@
+package Entities;
+
+public class ContaBancaria {
+    private int accountNumber;
+    private String holder;
+    private double balance;
+
+    public ContaBancaria(int accountNumber, String holder, double balance) {
+        this.accountNumber = accountNumber;
+        this.holder = holder;
+        this.balance = balance;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void deposit(double amount){
+        balance += amount;
+    }
+
+    public void withdraw(double amount){
+        balance -= amount;
+    }
+
+    public String toString(){
+        return "Account Number: "
+                + getAccountNumber()
+                + ", Holder: "
+                + getHolder()
+                + ", Balance: $ "
+                + getBalance();
+    }
+
+}
