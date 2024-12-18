01a49ff56190483af726d7045db11b81a558f144
Cody <ferreiradasilvajoaopedro730@gmail.com>
2024-12-17 23:40:06 -0300

    NumerosNegativosExercicie

---
+++ src/Application/NumerosNegativos.java
@@ -0,0 +1,29 @@
+package Application;
+
+import java.util.Scanner;
+
+public class NumerosNegativos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("how many numbers to enter? ");
+        int n = sc.nextInt();
+
+        int[] vect = new int[n];
+
+        for (int i = 0; i < vect.length; i++) {
+            System.out.print("Enter an number: ");
+            vect[i] = sc.nextInt();
+        }
+
+        System.out.println();
+        System.out.println("Negatives Numbers: ");
+        for (int j : vect) {
+
+            if (j < 0) {
+                System.out.println(j);
+            }
+
+        }
+    }
+}
