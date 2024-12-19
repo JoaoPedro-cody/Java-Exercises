a4dc4f09cf67a9ad7099727cb91e443a8cb6ad34
Cody <ferreiradasilvajoaopedro730@gmail.com>
2024-12-19 04:15:07 -0300

    BelowAverageExercise

---
+++ src/Application/AverageVector.java
@@ -0,0 +1,33 @@
+package Application;
+
+import java.util.Scanner;
+
+public class AverageVector {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many elements will the vector have? ");
+        byte elements = sc.nextByte();
+        double[] vect = new double[elements];
+
+        double sum = 0;
+        for (int i = 0; i < vect.length; i++) {
+            System.out.print("Enter an number: ");
+            vect[i] = sc.nextDouble();
+            sum += vect[i];
+        }
+
+        double average = sum / elements;
+
+        System.out.println();
+        System.out.printf("VECTOR AVERAGE: %.3f%n", average);
+        System.out.println("ELEMENTS BELOW AVERAGE: ");
+        for (int i = 0; i < vect.length; i++) {
+            if (vect[i] < average){
+                System.out.println(vect[i]);
+            }
+        }
+
+
+    }
+}
