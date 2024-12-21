6b210a1b5a645cf8e5f074afd9b6704cb2da5230
JoaoPedro-Cody <you@example.com>
2024-12-21 14:14:03 -0300

    SumElementsMain Add

--- src/Application/SumElementsMain.java
+++ src/Application/SumElementsMain.java
@@ -2,7 +2,7 @@ package Application;
 
 import java.util.Scanner;
 
-public class SumElements {
+public class SumElementsMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
